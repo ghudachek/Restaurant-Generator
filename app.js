f045302369c6b6429552282d624c45bdfc0acd13
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-01-26 15:35:43 -0600

    User search working

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 //let API_KEY = dc94f6e47c74ca3499a36e0541cb3e65
 
 
+
 //gets city ID:
 // https://developers.zomato.com/api/v2.1/cities?q=Nashville&apikey=dc94f6e47c74ca3499a36e0541cb3e65
+
+let userInput = document.querySelector('.search')
+let fetchButton = document.getElementById('find')
+
+
 async function getRestaurants(city) {
   try {
     //grab city id first...
@@ -16,6 +22,38 @@ async function getRestaurants(city) {
     //console.log(restaurantSearch.data)
 
 
+    restaurantSearch.data.restaurants.forEach(element => {
+
+      console.log(element.restaurant.name)
+      let restName = element.restaurant.name
+      console.log(element.restaurant.location.address)
+      let restAddress = element.restaurant.location.address
+      console.log(element.restaurant.timings)
+      let restHours = element.restaurant.timings
+      console.log(element.restaurant.url)
+      let restWebsite = element.restaurant.url
+      console.log(element.restaurant.user_rating.aggregate_rating)
+      let restRating = element.restaurant.user_rating.aggregate_rating
+      console.log(element.restaurant.phone_numbers)
+      let phoneNum = element.restaurant.phone_numbers
+      const restaurantInfo =
+        `
+    <li>
+    <h3>${restName}</h3>
+    <p>${restRating}</p>
+    <a>${restAddress}</a>
+    <a href=${phoneNum}>${phoneNum}</a>
+    <p>${restHours}</p>
+    <a href=${restWebsite}>${restName}'s Webstie</a>
+    </li>
+    `
+      const restInfoContainer = document.querySelector('.search-results')
+      restInfoContainer.insertAdjacentHTML("beforeend", restaurantInfo)
+
+    });
+
+
+
 
 
   } catch (err) {
@@ -24,7 +62,13 @@ async function getRestaurants(city) {
 
 }
 
-getRestaurants('Nashville')
+fetchButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(userInput.value)
+  getRestaurants(userInput.value)
+})
+
+
 
 
 //consdier grabbing lat and long from data.restaurant.location(.latitude/longitude) for map placement?
