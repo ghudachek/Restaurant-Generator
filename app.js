39d47a4e090ce85a3a83ff45f2632ded97e9caf0
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-01-26 15:00:29 -0600

    connect Js to HTML elements

--- app.js
+++ app.js
@@ -4,22 +4,18 @@
 
 //gets city ID:
 // https://developers.zomato.com/api/v2.1/cities?q=Nashville&apikey=dc94f6e47c74ca3499a36e0541cb3e65
-async function getCityId(city) {
+async function getRestaurants(city) {
   try {
+    //grab city id first...
     let response = await axios.get(`https://developers.zomato.com/api/v2.1/cities?q=${city}&apikey=dc94f6e47c74ca3499a36e0541cb3e65`)
-
+    //use city id to get restaurants...
     let cityId = response.data.location_suggestions[0].id
     //console.log(cityId) make sure it grabs the right one..
-
+    // made need a n option button here for user to select correcr location
     let restaurantSearch = await axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&apikey=dc94f6e47c74ca3499a36e0541cb3e65`)
-    console.log(restaurantSearch.data)
+    //console.log(restaurantSearch.data)
 
-    restaurantSearch.data.restaurants.forEach(element => {
-      console.log(element.restaurant.name)
-      console.log(element.restaurant.location.address)
-      console.log(element.restaurant.timings)
-      console.log(element.restaurant.url)
-    });
+
 
 
   } catch (err) {
@@ -28,7 +24,43 @@ async function getCityId(city) {
 
 }
 
-getCityId('Nashville')
+getRestaurants('Nashville')
+
+
+//consdier grabbing lat and long from data.restaurant.location(.latitude/longitude) for map placement?
 
 
 
+
+// Grab data for each Restaurant Array: name, location, hours, website  
+function getRestaurantsData() {
+  restaurantSearch.data.restaurants.forEach(element => {
+
+    console.log(element.restaurant.name)
+    let restName = element.restaurant.name
+    console.log(element.restaurant.location.address)
+    let restAddress = element.restaurant.location.address
+    console.log(element.restaurant.timings)
+    let restHours = element.restaurant.timings
+    console.log(element.restaurant.url)
+    let restWebsite = element.restaurant.url
+    console.log(element.restaurant.user_rating.aggregate_rating)
+    let restRating = element.restaurant.user_rating.aggregate_rating
+    console.log(element.restaurant.phone_numbers)
+    let phoneNum = element.restaurant.phone_numbers
+    const restaurantInfo =
+      `
+  <li>
+  <h3>${restName}</h3>
+  <p>${restRating}</p>
+  <a>${restAddress}</a>
+  <a href=${phoneNum}>${phoneNum}</a>
+  <p>${restHours}</p>
+  <a href=${restWebsite}>${restName}'s Webstie</a>
+  </li>
+  `
+    const restInfoContainer = document.querySelector('.search-results')
+    restInfoContainer.insertAdjacentHTML("beforeend", restaurantInfo)
+
+  });
+}
